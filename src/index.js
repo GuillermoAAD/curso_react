2bc7fb1ba4103d17f2ed72b6312ec9392355ba39
GuillermoAAD <guillermo.alcaraz@perspective.com.mx>
2023-01-27 15:53:52 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement);
+
+root.render(<h1>hola Mundo</h1>)
